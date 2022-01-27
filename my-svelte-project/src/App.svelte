e3ec102c42f2a95592d56eee001811ec68d920e9
damianof <damianof@users.noreply.github.com>
2022-01-27 14:58:55 -0500

    Chapter 02

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,65 +1,25 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  // import a reference to our ItemsList component
+  import ItemsListComponent from './components/items/ItemsList.component.svelte'
+
+  // mock some data:
+  const items = [{
+    id: 1,
+    name: 'Item 1',
+    selected: true
+  }, {
+    id: 2,
+    name: 'Item 2',
+    selected: false
+  }, {
+    id: 3,
+    name: 'Item 3',
+    selected: false
+  }]
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div class="home">
+    <ItemsListComponent items={items}/>
+  </div>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
---
+++ my-svelte-project/src/components/items/ItemsList.component.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  // expose a property called items with a default value of a blank array
+  export let items = []
+</script>
+
+<div>
+  <h3>My Items:</h3>
+  <ul>
+    {#each items as item}
+      <li>
+        {item.name}
+      </li>
+    {/each}
+  </ul>
+</div>

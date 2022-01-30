2176aaa35bdb1196f648b9c7655f4bb97eacd06a
Damiano <1753185+damianof@users.noreply.github.com>
2022-01-30 10:01:52 -0500

    Chapter 06 (with api consuming http client from chapter 7)

--- my-svelte-project/src/vite-env.d.ts
+++ my-svelte-project/src/vite-env.d.ts
@@ -1,2 +1,15 @@
+// file: src/vite-env.d.ts
+
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+// types for Vite env variables: 
+// (reference: https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript)
+interface ImportMetaEnv {
+  readonly VITE_API_CLIENT: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}

156ff9aef2f5b6d4c56721c43a7a2ebf9b3cc749
damianof <damianof@users.noreply.github.com>
2022-02-04 10:51:31 -0500

    Chapter 9 app config (updates)

--- my-svelte-project/src/api-client/mock/items/index.ts
+++ my-svelte-project/src/api-client/mock/items/index.ts
@@ -1,18 +1,17 @@
 // file: src/api-client/mock/items/index.ts
 
+import { config } from '../../../config' // <-- add this line
+
 import { 
   ItemsApiClientUrlsInterface, 
   ItemsApiClientInterface, 
   ItemsApiClientModel 
 } from '../../../models/api-client/items'
 
-const urls: ItemsApiClientUrlsInterface = {
-  fetchItems: '/static/data/items.json'
-}
 
 // instantiate the ItemsApiClient pointing at the url that returns static json mock data
 const itemsApiClient: ItemsApiClientInterface = new ItemsApiClientModel({ 
-  urls,
+  urls: config.items.apiUrls,
   mockDelay: 1000 // simulate a delay so we can see our loader
 })
 

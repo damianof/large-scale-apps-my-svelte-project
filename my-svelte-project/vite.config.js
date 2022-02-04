40d166ce8ea9fcdb1ab7543fa1012449fd987891
damianof <damianof@users.noreply.github.com>
2022-02-04 08:48:34 -0500

    cleanups

--- my-svelte-project/vite.config.js
+++ my-svelte-project/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  envDir: './src/'
+  envDir: './src/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
 })

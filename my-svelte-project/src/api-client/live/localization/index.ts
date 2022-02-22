1671db5b0028ad41d0a027c3d985e677c43286e7
damianof <damianof@users.noreply.github.com>
2022-02-22 15:15:31 -0500

    localization API client

---
+++ my-svelte-project/src/api-client/live/localization/index.ts
@@ -0,0 +1,19 @@
+// file: src/api-client/live/localization/index.ts
+
+// import a reference to the app config
+import { config } from '../../../config'
+
+import { 
+  LocalizationApiClientInterface, 
+  LocalizationApiClientModel 
+} from '../../../models/api-client/localization'
+
+// instantiate the LocalizationApiClient pointing at the url that returns static json mock data
+const localizationApiClient: LocalizationApiClientInterface = new LocalizationApiClientModel({ 
+  urls: config.localization.apiUrls
+})
+
+// export our instance
+export {
+  localizationApiClient
+}

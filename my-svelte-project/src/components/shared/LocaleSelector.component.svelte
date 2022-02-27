b63e9c40f69f4c82f93e68883df706c3d76070fb
damianof <damianof@users.noreply.github.com>
2022-02-27 14:41:27 -0500

    cleanup and code comments

---
+++ my-svelte-project/src/components/shared/LocaleSelector.component.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  //import { MessageFormatter } from 'svelte-i18n/types/runtime/types'
+  import { Readable } from 'svelte/store'
+
+
+  // expose component properties
+  export let locales: { key: string }[] = []
+  export let currentLocale: string = ''
+  export let onLocaleClick: Function
+  export let t: Function
+</script>
+
+
+<div class="locale-selector">
+  {#each locales as item}
+    <label class="cursor-pointer">
+      <input type="radio" group={currentLocale} name="locale" value={item.key} checked={ currentLocale === item.key } on:click={() => onLocaleClick(item.key)} />
+      { t(`locale.selector.${ item.key }`) }
+    </label>
+  {/each}
+</div>

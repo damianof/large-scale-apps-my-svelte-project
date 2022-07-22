349af89ba610bc661478f4cce1610a0de97fffd3
damianof <damianof@users.noreply.github.com>
2022-07-22 10:24:31 -0400

    Chapter 17 progress

---
+++ my-component-library/src/components/index.ts
@@ -0,0 +1,6 @@
+import SampleComp from './sample-component/SampleComp.svelte'
+import Counter from './counter/Counter.svelte'
+
+export { 
+  SampleComp
+}
---
+++ my-component-library/src/components/sample-component/SampleComp.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+  // expose a property called testid
+  export let testid = 'not-set'
+  // expose a property called title
+  export let title = 'not-set'
+
+  // a computed property to return the css class 
+  $: cssClass = (): string => {
+    return `p-2 border border-green-500 ${ title }`
+  }
+
+</script>
+
+<div data-testid={testid} class={cssClass()}>
+  <span>{ title }</span>
+</div>
